bc4d323b88188758ec69ebd01ab472ea9a6c9fbe
vagrant <vagrant@midsunbox>
2023-10-11 16:10:42 +0000

    git version

--- nvimInstall.sh
+++ nvimInstall.sh
@@ -2,6 +2,9 @@
 # install nvim
 if ! [ -f /usr/bin/nvim ]; then
     apt install ripgrep
+    sudo apt-add-repository ppa:git-core/ppa
+    sudo apt-get update
+    sudo apt-get install git
     curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
     chmod u+x nvim.appimage
     ./nvim.appimage --appimage-extract
