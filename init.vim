d8e811ad5ce5f1e118c8245661daf1d46a0a06d3
Preston Dresser <pgdresse@uwaterloo.ca>
2024-07-12 15:04:31 -0400

    update plugins.lua

--- init.vim
+++ init.vim
@@ -1,7 +1,8 @@
 imap jj <Esc>
+tnoremap jj <C-\><C-n>
 
-set tabstop=4
-set shiftwidth=4
+set tabstop=2
+set shiftwidth=2
 set expandtab
 
 lua require("plugins")
@@ -9,3 +10,4 @@ lua require("plugins")
 nmap <C-t> :Telescope <Enter>
 nmap <C-p> :Telescope find_files <Enter>
 nmap <C-F> :Telescope live_grep <Enter>
+
--- plugins.lua
+++ plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PACKER_BOOTSTRAP = fn.system({
-		"git",
-		"clone",
-		"--depth",
-		"1",
-		"https://github.com/wbthomason/packer.nvim",
-		install_path,
-	})
-	print("Installing packer close and reopen Neovim...")
-	vim.cmd([[packadd packer.nvim]])
+        PACKER_BOOTSTRAP = fn.system({
+                "git",
+                "clone",
+                "--depth",
+                "1",
+                "https://github.com/wbthomason/packer.nvim",
+                install_path,
+        })
+        print("Installing packer close and reopen Neovim...")
+        vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,31 +26,46 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-	return
+        return
 end
 
 -- Have packer use a popup window
 packer.init({
-	display = {
-		open_fn = function()
-			return require("packer.util").float({ border = "rounded" })
-		end,
-	},
+        display = {
+                open_fn = function()
+                        return require("packer.util").float({ border = "rounded" })
+                end,
+        },
 })
 
 -- Install your plugins here
 return packer.startup(function(use)
-	use ("wbthomason/packer.nvim") -- Have packer manage itself	
+    use ("wbthomason/packer.nvim") -- Have packer manage itself
 
-    use("nvim-lua/plenary.nvim") -- Lua functions that Telescope plugin use
+    use("folke/tokyonight.nvim")
+    use("nvim-treesitter/nvim-treesitter")
+    use("nvim-lua/plenary.nvim")
     use("BurntSushi/ripgrep")
     use("nvim-telescope/telescope.nvim")
     use("sindrets/Diffview.nvim")
     use("preservim/nerdtree")
+    use("kevinhwang91/promise-async")
+    use("kevinhwang91/nvim-ufo")
 
+    vim.o.foldcolumn = '1' -- '0' is not bad
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
 
-
-	if PACKER_BOOTSTRAP then
-		require("packer").sync()
-	end
+    if PACKER_BOOTSTRAP then
+      require("packer").sync()
+    else
+      require('ufo').setup({
+        provider_selector = function(bufnr, filetype, buftype)
+            return {'treesitter', 'indent'}
+        end
+      })
+      vim.cmd [[colorscheme tokyonight-night]]
+    end
 end)
+
