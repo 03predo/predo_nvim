946cb6bc9fafd9bc1adbf1c78a2603fed3416cef
03predo <98138404+03predo@users.noreply.github.com>
2024-09-07 11:18:23 -0400

    Update nvimInstall.sh

--- nvimInstall.sh
+++ nvimInstall.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # install nvim
 if ! [ -f /usr/bin/nvim ]; then
+    # TODO: update to curl ripgrep, should be able to curl and add to path and telescope still pick it up
+    # curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz'
+    # tar xf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
     apt install ripgrep
     sudo apt-add-repository ppa:git-core/ppa
     sudo apt-get update
