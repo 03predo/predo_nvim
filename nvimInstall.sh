451b37d035414be01c4292aea855f84602b7c09b
Preston Dresser <pgdresse@uwaterloo.ca>
2024-09-25 08:35:06 -0400

    update script

--- nvimInstall.sh
+++ nvimInstall.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
+
 # install nvim
-if ! [ -f /usr/bin/nvim ]; then
-    # TODO: update to curl ripgrep, should be able to curl and add to path and telescope still pick it up
-    # curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz'
-    # tar xf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
-    apt install ripgrep
-    sudo apt-add-repository ppa:git-core/ppa
-    sudo apt-get update
-    sudo apt-get install git
-    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-    chmod u+x nvim.appimage
+if ! [ -f $HOME/.local/bin/nvim ]; then
+    curl -LO 'https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz'
+    tar xf ripgrep-13.0.0-x86_64-unknown-linux-musl.tar.gz
+    ln -s $PWD/ripgrep-13.0.0-x86_64-unknown-linux-musl/rg ~/.local/bin/rg
+
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 
+    chmod +x $PWD/nvim.appimage
     ./nvim.appimage --appimage-extract
     ./squashfs-root/AppRun --version
-    ln -s $PWD/squashfs-root/AppRun /usr/bin/nvim
+    ln -s $PWD/squashfs-root/AppRun ~/.local/bin/nvim
+    echo "export PATH="\$HOME/.local/bin:\$PATH"" >> $HOME/.bashrc
 fi
 
+# config nvim
+mkdir -p ~/.config
+
+if [ -f ~/.config/nvim ]; then
+  rm -rf ~/.config/nvim
+fi
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ln -s $SCRIPT_DIR ~/.config/nvim
