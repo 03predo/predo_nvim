6e668dbf0db7467a78d7f977cbcebc47c0d1b04d
Preston Dresser <pgdresse@uwaterloo.ca>
2024-09-24 22:30:39 -0400

    transition to nvim-cmp

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -53,14 +53,15 @@ return {
     "kevinhwang91/nvim-ufo",
     dependencies = {
       "kevinhwang91/promise-async",
-      "neoclide/coc.nvim",
+      "neovim/nvim-lspconfig",
     },
-    opts = function()
+    opts = function() 
+      require'lspconfig'.clangd.setup{}
+
       vim.o.foldcolumn = '1' -- '0' is not bad
       vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
       vim.o.foldlevelstart = 99
       vim.o.foldenable = true
     end,
   },
-
 }
